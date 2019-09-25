11b6b0a139c5d0909382e3d020d18d9a2c0aa6d3
Jan <j.rzymkowski@gmail.com>
2019-09-25 18:04:51 +0200

    Added get_friend_list()

--- get_films.py
+++ get_films.py
@@ -21,8 +21,52 @@ def wait_for_login(driver):
     except:
         pass
 
-def get_friend_list(driver):
+def get_my_username(driver):
     pass
+
+def get_friend_list(driver):
+    ##assumes driver already at the friends page
+    nicks = []
+    time.sleep(3)
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(1)
+    found = False
+    friends = []
+    try:
+        section = driver.find_element_by_class_name("section__userFriends")
+
+        friends = section.find_elements_by_class_name("user__body")
+        print(str(len(friends)) + " found friends")
+        if len(friends) > 0:
+            found = True
+
+    except NoSuchElementException:
+        if not found:
+            print("Friends not found")
+            return []
+
+    for f in friends:
+        href, fn, ln, user_link = "not_found", "not_found", "not_found", "not_found"
+        try:
+            href = f.find_element_by_class_name("user__name").get_attribute("href")
+            fn = f.find_element_by_class_name("user__firstName").text
+            ln = f.find_element_by_class_name("user__lastName").text
+        except NoSuchElementException:
+            pass
+        try:
+            user_link = f.find_element_by_class_name("user__link").text
+            if user_link != "":
+                nicks.append(user_link)
+            else:
+                nicks.append(href.split("/")[-1])
+        except NoSuchElementException:
+            nicks.append(href.split("/")[-1])
+
+    if nicks[-1] == "friends#":
+        return nicks[-1]
+    else:
+        return nicks
 
 def link_to_next_page(driver):
     try:
@@ -75,13 +119,18 @@ def get_ratings_starting(driver, url, friend_nick = ""):
 
 try:
     wait_for_login(driver)
-    films_data = get_ratings_starting(driver, "https://www.filmweb.pl/user/Mikolaj_Kastor/films?page=10", friend_nick="MK")
+    # films_data = get_ratings_starting(driver, "https://www.filmweb.pl/user/Mikolaj_Kastor/films?page=10", friend_nick="MK")
+    #
+    # fieldnames = list(films_data[0].keys())
+    # with open('films.csv', 'w', newline='') as output_file:
+    #     dict_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+    #     dict_writer.writeheader()
+    #     dict_writer.writerows(films_data)
 
-    fieldnames = list(films_data[0].keys())
-    with open('films.csv', 'w', newline='') as output_file:
-        dict_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
-        dict_writer.writeheader()
-        dict_writer.writerows(films_data)
+    driver.get("https://www.filmweb.pl/user/j_rzymkowski/friends")
+    friends = get_friend_list(driver)
+    print(len(friends))
+    print(friends)
 
 finally:
     driver.quit()
